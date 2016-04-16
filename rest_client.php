38910b7d2d47393311e014318d55b657e362cc3d
Ismail NGUYEN <me@nguyenismail.com>
2016-04-16 17:44:06 +0200

    initial commit of RestClient class

---
+++ rest_client.php
@@ -0,0 +1,21 @@
+<?php
+class RestClient
+{
+    private $_protocol = null;
+    private $_host = null;
+    private $_port = null;
+    private $_user = null;
+    private $_pass = null;
+	private $_method = null;
+	private $_url = null;
+	private $_params = array();
+	private $_headers = array();
+	
+	protected function __construct($host, $port, $protocol)
+    {   
+        $this->_host     = $host;
+        $this->_port     = $port;
+        $this->_protocol = $protocol;
+    }
+}
+?>
