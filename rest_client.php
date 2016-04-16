a897ad61f37e1cdff728d3ab9b87231942ba9fd4
Ismail NGUYEN <me@nguyenismail.com>
2016-04-16 17:53:14 +0200

    public methods for get/post/put/delete, authentification, connection and parameters

--- rest_client.php
+++ rest_client.php
@@ -19,5 +19,61 @@ class RestClient
         $this->_port     = $port;
         $this->_protocol = $protocol;
     }
+	
+	static public function connect($host, $port = 80, $protocol = Rest::HTTP)
+    {
+        return new self($host, $port, $protocol);
+    }
+   
+    public function param($key, $value = null)
+    {
+        $this->_params[$key] = $value;
+        return $this;
+    }
+       
+    public function authentification($user, $pass)
+    {
+        $this->_user = $user;
+        $this->_pass = $pass;
+		return $this;
+    }
+	
+    public function post($url  = null)
+    {
+        return $this->_buildRequest(Rest::POST, $url);
+    }
+	
+	public function get($url = null)
+    {
+        return $this->_buildRequest(Rest::GET, $url);
+    }
+	
+	public function put($url = null)
+    {
+        return $this->_buildRequest(Rest::PUT, $url);
+    }
+	
+	public function delete($url = null)
+    {
+        return $this->_buildRequest(Rest::DELETE, $url);
+    }
+	
+	public function setHeaders($headers)
+    {
+        $this->_headers = $headers;
+        return $this;
+    }
+	
+	private function _buildRequest($method, $url) {
+		$this->_method = $method;
+		$this->_url = $this->_buildUrl($url);
+		
+		return $this;
+	}
+	
+	private function _buildUrl($url = null)
+    {
+        return "{$this->_protocol}://{$this->_host}:{$this->_port}/{$url}";
+    }
 }
 ?>
